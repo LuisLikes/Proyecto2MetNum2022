b74394bed5bfe45a5d5c1efaf323ba0ddc27546f
nefta2 <92494202+nefta2@users.noreply.github.com>
2022-05-31 18:22:50 -0500

    Update interface_b.py
    Se introduce posibilidad de ver gráfica

--- interface_b.py
+++ interface_b.py
@@ -1,9 +1,4 @@
-from typing_extensions import Self
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg,
-    NavigationToolbar2Tk
-)
-from matplotlib.figure import Figure
+from doctest import master
 import numpy as np
 from cProfile import label
 from ctypes import alignment
@@ -17,8 +12,10 @@ import PIL.Image
 import PIL.ImageTk
 from numpy import bitwise_xor
 import matplotlib.pyplot as plt
-plt.use('TkAgg')
-# Funciones
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg)
+from sympy import true
 
 # Tkinter Interface
 bisec = Tk()
@@ -36,28 +33,17 @@ def deleteTab():
 
 def tab2():
     def calcGraf():
-        # 100 linearly spaced numbers
-        x = np.linspace(-5, 5, 100)
-
-        # the function, which is y = x^2 here
-        y = x**3+x**2+4
-
-        # setting the axes at the centre
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
+        x = np.linspace(-10, 10, 100)
+        y = -0.5*x**2+2.5*x+4.5
+        fig = plt.figure(figsize=(5, 4), dpi=100)
+        ax = fig.add_subplot(111)
         ax.spines['left'].set_position('center')
         ax.spines['bottom'].set_position('zero')
         ax.spines['right'].set_color('none')
         ax.spines['top'].set_color('none')
-
-        # plot the function
-        plt.plot(x, y, 'r')
-
-        graph = Figure(figsize=(6, 4), dpi=100)
-        graph_canvas = FigureCanvasTkAgg(graph, self)
-        NavigationToolbar2Tk(graph_canvas, self)
-        axes = graph.add_subplot()
-        graph_canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        ax = plt.plot(x, y, 'r')
+        ax = FigureCanvasTkAgg(fig, master=bisec)
+        ax.get_tk_widget().place(x=30, y=200)
 
     deleteTab()
     bisec = Tk()
@@ -65,44 +51,44 @@ def tab2():
     bisec.geometry("700x550")
     bisec.config(background="#272635")
     encabezado = Label(bisec, text="Introduce valores en la ecuación", fg="#A6A6A8", bg="#272635",
-                       font=("Berlin Sans FB Demi", 20)).place(x=150, y=50)
+                       font=("Berlin Sans FB Demi", 20)).place(x=150, y=20)  # 50
 
     # Ecuación
 
     # dato de x2
     x2data = StringVar()
     valorx2 = Entry(width=2, textvariable=x2data, font=(
-        "Berlin Sans FB Demi", 17)).place(x=250, y=120)
+        "Berlin Sans FB Demi", 17)).place(x=250, y=90)
     x2 = Label(bisec, text="X +", fg="#A6A6A8", bg="#272635",
-               font=("Berlin Sans FB Demi", 20)).place(x=280, y=119)
+               font=("Berlin Sans FB Demi", 20)).place(x=280, y=89)
     # exponente de x2
     exp2data = StringVar()
     exponentex2 = Entry(width=2, textvariable=exp2data, font=(
-        "Berlin Sans FB Demi", 10)).place(x=300, y=100)
+        "Berlin Sans FB Demi", 10)).place(x=300, y=70)
 
     # dato de x1
     x1data = StringVar()
     valorx1 = Entry(width=2, textvariable=x1data, font=(
-        "Berlin Sans FB Demi", 17)).place(x=325, y=120)
+        "Berlin Sans FB Demi", 17)).place(x=325, y=89)
     x1data = Label(bisec, text="X +", fg="#A6A6A8", bg="#272635",
-                   font=("Berlin Sans FB Demi", 20)).place(x=355, y=119)
+                   font=("Berlin Sans FB Demi", 20)).place(x=355, y=89)
     # exponente de x1
     exp1data = StringVar()
     exponentex1 = Entry(width=2, textvariable=exp1data, font=(
-        "Berlin Sans FB Demi", 10)).place(x=375, y=100)
+        "Berlin Sans FB Demi", 10)).place(x=375, y=70)
 
     # dato de último término
     valor = Entry(width=2, font=(
-        "Berlin Sans FB Demi", 17)).place(x=400, y=119)
+        "Berlin Sans FB Demi", 17)).place(x=400, y=89)
     xdata = Label(bisec, text="X", fg="#A6A6A8", bg="#272635",
-                  font=("Berlin Sans FB Demi", 20)).place(x=430, y=119)
+                  font=("Berlin Sans FB Demi", 20)).place(x=430, y=89)
     # exponente de x
     expdata = StringVar()
     exponentex = Entry(width=2, textvariable=exp1data, font=(
-        "Berlin Sans FB Demi", 10)).place(x=450, y=100)
+        "Berlin Sans FB Demi", 10)).place(x=450, y=70)
 
     vergráfico = Button(bisec, text="Ver gráfico", fg="#FFFFFF", bg="#E4572E", font=("Berlin Sans FB Demi", 10),
-                        height=2, width=11, command=calcGraf).place(x=310, y=170)
+                        height=2, width=11, command=calcGraf).place(x=310, y=140)
 
 
 def tab1():
