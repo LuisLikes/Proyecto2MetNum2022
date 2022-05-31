79a8fc1cfd3958303f5a5a24e31e1b33e19ad015
nefta2 <92494202+nefta2@users.noreply.github.com>
2022-05-31 14:40:27 -0500

    Escape

--- interface_b.py
+++ interface_b.py
@@ -9,6 +9,8 @@ from tkinter import messagebox
 import PIL.Image
 import PIL.ImageTk
 from numpy import bitwise_xor
+import matplotlib.pyplot as plt
+
 
 # Funciones
 
@@ -27,11 +29,42 @@ def deleteTab():
 
 
 def tab2():
+    def calcGraf():
+        print("hola")
     deleteTab()
     bisec = Tk()
     bisec.title("Bisección Gay")
     bisec.geometry("700x550")
     bisec.config(background="#272635")
+    encabezado = Label(bisec, text="Introduce valores en la ecuación", fg="#A6A6A8", bg="#272635",
+                       font=("Berlin Sans FB Demi", 20)).place(x=150, y=50)
+
+    # Ecuación
+    # dato de x2
+    x2data = StringVar()
+    valorx2 = Entry(width=2, textvariable=x2data, font=(
+        "Berlin Sans FB Demi", 17)).place(x=250, y=120)
+    x2 = Label(bisec, text="X +", fg="#A6A6A8", bg="#272635",
+               font=("Berlin Sans FB Demi", 20)).place(x=280, y=119)
+    # exponente de x2
+    exp2data = StringVar()
+    exponentex2 = Entry(width=2, textvariable=exp2data, font=(
+        "Berlin Sans FB Demi", 10)).place(x=300, y=100)
+    # dato de x1
+    x1data = StringVar()
+    valorx1 = Entry(width=2, textvariable=x1data, font=(
+        "Berlin Sans FB Demi", 17)).place(x=325, y=120)
+    x1data = Label(bisec, text="X +", fg="#A6A6A8", bg="#272635",
+                   font=("Berlin Sans FB Demi", 20)).place(x=355, y=119)
+    # exponente de x1
+    exp1data = StringVar()
+    exponentex1 = Entry(width=2, textvariable=exp1data, font=(
+        "Berlin Sans FB Demi", 10)).place(x=375, y=100)
+    # dato de último término
+    valor = Entry(width=2, font=(
+        "Berlin Sans FB Demi", 17)).place(x=400, y=120)
+    vergráfico = Button(bisec, text="Ver gráfico", fg="#FFFFFF", bg="#E4572E", font=("Berlin Sans FB Demi", 10),
+                        height=2, width=11).place(x=310, y=170)
 
 
 def tab1():
